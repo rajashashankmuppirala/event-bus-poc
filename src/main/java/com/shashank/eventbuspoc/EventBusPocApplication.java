1599e4ba3fd13102dde989b67d349535ca6f4cf4
Raja Shashank Muppirala <mshashank@gmail.com>
2025-09-25 08:18:58 -0400

    first commit

---
+++ src/main/java/com/shashank/eventbuspoc/EventBusPocApplication.java
@@ -0,0 +1,13 @@
+package com.shashank.eventbuspoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventBusPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventBusPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shashank/eventbuspoc/EventBusPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shashank.eventbuspoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventBusPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
